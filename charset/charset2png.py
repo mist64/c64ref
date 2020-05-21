8d4c9f277eb1c4c8948da4fbe48bff0a18d485d9
Michael Steil <mist64@mac.com>
2020-05-21 18:48:06 +0200

    added all zimmers.net character ROMs

--- charset/charset2png.py
+++ charset/charset2png.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
 
+import sys
 from PIL import Image, ImageDraw
 
-charset = open("chargen", "rb").read()
+if len(sys.argv) < 2:
+	print("Usage: {} <charset.bin>".format(sys.argv[0]))
+	exit(1)
 
-img = Image.new('1', (16 * 8, 16 * 8), color = 'white')
-pixels = img.load()
-for c in range(0, 256):
-	for y in range(0, 8):
-		for x in range(0, 8):
-			if charset[((c << 3) | y)] & (1 << (7-x)) > 0:
-				pixels[((c & 15) * 8) | x, (c >> 4) * 8 | y] = 0
+for filename_in in sys.argv[1:]:
+	if filename_in[-4:] == '.bin' or filename_in[-4:] == '.rom':
+		filename_base = filename_in[:-4]
+	else:
+		filename_base = filename_in
 
-img.save("charset-c64.png")
+	print("Reading {}".format(filename_in))
+	charset = open(filename_in, "rb").read()
+
+	start_offset = 0
+	index = 0
+
+	while start_offset < len(charset):
+		img = Image.new('1', (16 * 8, 16 * 8), color = 'white')
+		pixels = img.load()
+		for c in range(0, 256):
+			for y in range(0, 8):
+				for x in range(0, 8):
+					if charset[start_offset + ((c << 3) | y)] & (1 << (7-x)) > 0:
+						pixels[((c & 15) * 8) | x, (c >> 4) * 8 | y] = 0
+
+		filename_out = filename_base + '_{}.png'.format(index)
+		print("Writing {}".format(filename_out))
+		img.save(filename_out)
+
+		start_offset += 2048
+		index += 1
+
 
