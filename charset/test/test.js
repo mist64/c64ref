9c1fa6095b43c0a808e18ce9b6acb736b00e8aa1
Michael Steil <mist64@mac.com>
2020-06-02 17:00:18 +0200

    charset: SVG WIP

--- charset/test/test.js
+++ charset/test/test.js
@@ -8,23 +8,49 @@ function init() {
 		var arrayBuffer = req.response;
 		if (arrayBuffer) {
 			var byteArray = new Uint8Array(arrayBuffer);
-			createSVG(byteArray.slice(0,8));
+			all_svg = "";
+			for (i = 0; i < 10; i++) {
+				svg = svgFromCharBin(byteArray.slice(i*8,i*8+8));
+				all_svg += svg;
+			}
+			var infoBox = document.getElementById("main");
+			infoBox.innerHTML = all_svg;
 		}
 	};
 	req.send(null);
 }
 
-function createSVG(bin) {
-	var infoBox = document.getElementById("main");
+function svgFromCharBin(bin) {
 	svg = "";
+	svg = "<svg shape-rendering=\"crispEdges\" viewBox=\"0 -.5 8 8\" xmlns=\"http://www.w3.org/2000/svg\"><path stroke=\"#000\" d=\"";
 	for (y = 0; y < 8; y++) {
 		byte = bin[y];
+		is_first = true;
+		prev = -1;
+		count = 1;
 		for (x = 0; x < 8; x++) {
 			bit = (byte >> (7-x)) & 1;
-			svg += bit + ",";
+			//svg += "bit #" + x + ": " + bit + "<br/>"
+			if (bit == prev) {
+				count++;
+			} else {
+				if (prev != -1) {
+//					svg += "found " + count + " bits of value " + prev + "<br/>"
+				}
+				if (prev == 1) {
+					svg += "M" + (x - count) + " " + y + "h" + count;
+				}
+				prev = bit;
+				count = 1;
+			}
 		}
-		svg += "<br/>";
+//		svg += "found " + count + " bits of value " + prev + "<br/>"
+				if (prev == 1) {
+					svg += "M" + (x - count) + " " + y + "h" + count;
+				}
+//		svg += "new line<br/>"
 	}
-	infoBox.innerHTML = svg;
-
+//	svg += "m2 0h4m5 0h2m4 0h5m4 0h4m3"
+	svg += "\"/></svg>";
+	return svg;
 }
