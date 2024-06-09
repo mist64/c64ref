66ab2e65f9dd57fc5e14ce257b11553943cd16ad
Lisa Brodner <ellduin@me.com>
2024-06-10 00:01:33 +0200

    documenting, simplifying a bit, cleaning up

--- generate.py
+++ generate.py
@@ -142,6 +142,24 @@ BASIC_HTML = """
 
 ### FUNCTIONS for things that are longer
 
+def add_github_corner(soup):
+
+	#
+	# add a "github corner" with a waving octocat to the top right
+	# html source via: http://tholman.com/github-corners/
+	#
+	html_doc = """
+<a href="https://github.com/mist64/c64ref" class="github-corner" aria-label="View source on GitHub">
+  <svg width="80" height="80" viewBox="0 0 250 250" style="fill:var(--main-color); color:#fff; position: absolute; top: 0; border: 0; right: 0; clip-path: polygon(0 0, 100% 0, 100% 100%);" aria-hidden="true">
+	<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
+	<path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
+	<path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
+  </svg>
+</a>"""
+	tag = soup.find(id="cat")
+	tag_append_tag(tag, html_doc)
+
+
 def add_navigation(cc):
 	soup = cc.soup
 
@@ -214,6 +232,7 @@ def add_main_content_from_subdirectories(cc):
 		path = 	os.path.join(cc.source_path, cc.category.generator_name)
 		with open(path, 'r') as file:
 			output_str = file.read()
+	#
 	# or exit
 	else:
 		output_str = ""
@@ -239,17 +258,14 @@ def add_main_content_from_subdirectories(cc):
 	# extract the relevant infos from the generated HTMLs
 	#
 	# main content <div>
-	#
 	tag = cc.soup.find("main")
 	src_tag = src_soup.find("div", class_="content")
 	tag.append(src_tag)
 	#
 	# position in <head> tag at which to insert the additional files:
-	#
 	tag = cc.soup.find("link")
 	#
 	# transfer all <style>, <script> and <link> tags
-	#
 	for src_tag in src_soup.find_all('style'):
 		tag.insert_after(src_tag)
 
@@ -277,7 +293,6 @@ def copy_resources_to_build_dir(cc):
 
 	#
 	# make list of files matching the categories generator_patterns
-	#
 	patterns = cc.category.generator_patterns
 	if patterns:
 		filtered_files = []
@@ -300,30 +315,13 @@ def copy_resources_to_build_dir(cc):
 
 	#
 	# for debugging:
-	#     write a .txt containing a list of the unmatched/uncopied files
-	#
+	#     write a .txt containing a list of the unmatched files
 	filename = os.path.join(cc.dest_path_tmp, "files_no_copy.txt")
 	with open(filename, 'w', encoding='utf-8') as file:
 		for unmatched_file in unmatched_files:
 			# ignore .DS_Store and the index.html
 			if unmatched_file != "index.html" and unmatched_file != ".DS_Store":
 				file.write(f"{unmatched_file}\n")
-
-
-### OCTOCAT
-
-def add_github_corner(soup):
-	# via: http://tholman.com/github-corners/
-	html_doc = """
-<a href="https://github.com/mist64/c64ref" class="github-corner" aria-label="View source on GitHub">
-  <svg width="80" height="80" viewBox="0 0 250 250" style="fill:var(--main-color); color:#fff; position: absolute; top: 0; border: 0; right: 0; clip-path: polygon(0 0, 100% 0, 100% 100%);" aria-hidden="true">
-    <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
-    <path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
-    <path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path>
-  </svg>
-</a>"""
-	tag = soup.find(id="cat")
-	tag_append_tag(tag, html_doc)
 
 
 ### HELPER
@@ -347,11 +345,12 @@ def ensured_path(path, *paths, is_dir):
 
 
 
-### MAIN
+##################### MAIN #####################
 
 ##
 ## SETUP
 ##
+print("*** Setup")
 
 #
 # parsing command line arguments
@@ -381,13 +380,19 @@ CONFIG.git_branch_name = branch_name
 if CONFIG.deploy:
 
 	if CONFIG.git_has_changes:
-		print("Generate and upload failed:")
+		print("Generating and upload failed:")
 		print("There are uncommited changes in the working copy.")
 		exit()
 
+	CONFIG.fast_build = False # reset for uploading
+
 	if branch_name == "main":
+		CONFIG.build_wips = False # reset for uploading to main
+
 		print(f"{CONFIG.git_branch_name}:")
+		print(f"  [{'|'.join([category.short_title for category in CATEGORIES])}]")
 		response = input("Deploy to production? [Y/N]: ").strip()
+		exit()
 		if response.lower() != 'y':
 			print("Exiting.")
 			exit()
@@ -407,9 +412,9 @@ ensured_path(CONFIG.build_dir_tmp, is_dir=True)
 ##
 ## GENERATE HTML in build_dir
 ##
-
 print("*** Generating")
 
+#
 # copy global resources: stylesheet
 shutil.copy(os.path.join(CONFIG.source_dir, "style.css"), CONFIG.build_dir)
 
@@ -439,13 +444,10 @@ for category in CATEGORIES:
 		tag = soup.find(id="headline")
 		tag.string = category.long_title
 
+		add_github_corner(soup)
 		add_navigation(cc)
 		add_byline_and_build_info(cc)
-		add_github_corner(soup)
-
 		add_main_content_from_subdirectories(cc)
-
-		# write and copy to build directory
 		copy_resources_to_build_dir(cc)
 
 		#
