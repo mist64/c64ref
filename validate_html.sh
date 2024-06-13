877cfcc6fd57f16290430e6238932b4a9935e153
Lisa Brodner <ellduin@me.com>
2024-06-13 21:10:39 +0200

    updated validate, fixed a bug where all the warnings and errors were filtered out

--- validate_html.sh
+++ validate_html.sh
@@ -3,8 +3,11 @@
 # Using https://validator.github.io/validator/
 
 
-DIRECTORY_PATH="out"
-REGEXP=".*(Trailing.slash|Consider.adding.a..lang.|Consider.using.the..h1..element.as.a.top.level.heading.only|Stray.end.tag|Duplicate.ID|The.first.occurrence.of.ID|row.group.established.by.a|exceeded.the.column.count.established|not.allowed.as.child.of.element|Use.CSS.instead|).*"
+DIRECTORY_PATH="out out_unmodified"
+
+REGEXP=".*(Trailing.slash|Consider.adding.a..lang.|Consider.using.the..h1..element.as.a.top.level.heading.only|Stray.end.tag|Duplicate.ID|The.first.occurrence.of.ID|row.group.established.by.a|exceeded.the.column.count.established|not.allowed.as.child.of.element|Use.CSS.instead|Unclosed.element|Bad.character|but.there.were.open.elements|cannot.be.represented.as.XML|A.slash.was.not.immediately.followed.by|not.allowed.on.element|violates.nesting.rules|is.not.serializable.as.XML|Probable.causes..Unescaped|Probable.cause..Matching.quote.missing|missing.immediately.before|Probable.cause..Missing|an.element.of.the.same.type.was.already.open|Empty.heading|No.space.between.attributes|Duplicate.attribute).*"
+
+
 #REGEXP="" # no filter, all errors/warnings
 
 # show info/warnings and errors
@@ -20,8 +23,8 @@ echo $HTML_PARAMS
 echo "HTML:"
 vnu --skip-non-html $HTML_PARAMS
 
-echo -e "\n\n\n CSS:"
-vnu --skip-non-css  $PARAMS
-
-echo -e "\n\n\n SVG:"
-vnu --skip-non-svg  $PARAMS
+# echo -e "\n\n\n CSS:"
+# vnu --skip-non-css  $PARAMS
+#
+# echo -e "\n\n\n SVG:"
+# vnu --skip-non-svg  $PARAMS
