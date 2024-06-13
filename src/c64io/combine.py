6526c54cebe2b2fd8fe744bcb1bebccea2fc9339
Lisa Brodner <ellduin@me.com>
2024-06-13 20:32:05 +0200

    removed trailing slashes for br and hr

--- src/c64io/combine.py
+++ src/c64io/combine.py
@@ -184,7 +184,7 @@ print('<body>')
 print('<div class="content">')
 print('<div>')
 
-print('<b>This allows you to view different commentaries side-by-side. You can enable/disable individual columns:</b><br/><br/>')
+print('<b>This allows you to view different commentaries side-by-side. You can enable/disable individual columns:</b><br><br>')
 print('<table class="checkbox_table">')
 for i in range(0, len(filenames)):
 	print('<tr><td><input type="checkbox" id="checkbox_' + str(i) + '" checked onclick="hideCol(' + str(i) + ', document.getElementById(\'checkbox_' + str(i) + '\').checked);" /></td><td style="white-space: nowrap;"><b><a href="' + links[i] + '">' + names[i] + '</a></b></td><td>' + descriptions[i] + '</td></tr>')
@@ -193,7 +193,7 @@ print('</table>')
 print('<p>')
 print('<input type="checkbox" id="checkbox_decimal" name="checkbox_decimal" onclick="toggleDecimal(document.getElementById(\'checkbox_decimal\').checked);" />')
 print('<label for="checkbox_decimal">Show Decimal Address</label>')
-print('<br />')
+print('<br>')
 print('<button id="toggle_details_button" onclick="closeAll()">Hide All Details</button>')
 print('</p>')
 
@@ -371,7 +371,7 @@ while(True):
 			previous_heading = ''
 			for heading in headings:
 				if previous_heading.endswith('.'):
-					heading = '<br/>' + heading
+					heading = '<br>' + heading
 				html_heading = markdown.markdown(heading)
 				html_heading.replace('<p>', '')
 				html_heading.replace('</p>', '')
