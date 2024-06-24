6b8b214b5612b982a308e09e9a6ad4ed2923266d
Lisa Brodner <ellduin@me.com>
2024-06-24 20:30:17 +0200

    WIP - changed the c64mem script to be usable with configurations and removed the generated script code

--- src/c64mem/script.js
+++ src/c64mem/script.js
@@ -1,27 +1,52 @@
-window.onload = init;
+/*
+// Add this to the using HTML with the relevant config changes:
+<script>
+const pageConfiguration = {
+	storage_prefix: "com.pagetable.c64mem.",
+	number_of_entries: 8,
+	number_of_header_columns: 3,
+	has_decimal_column: true,
+	decimal_column: 2
+};
+</script>
+<script src="script.js"></script>
+};
+*/
+
+// Global variable to store the configuration
+let config;
+
+window.onload = function() {
+	config = pageConfiguration;
+	//console.log(config);
+	init()
+};
 
 function init() {
 	var tbl = document.getElementById("disassembly_table");
-	for (var i = 0; i < 8; i++) {
-		var key = "com.pagetable.c64mem.column_" + i;
+	for (var i = 0; i < config.number_of_entries; i++) {
+		var key = config.storage_prefix + "column_" + i;
 		var element_name = "checkbox_" + i;
 		var checked = localStorage.getItem(key) != "hidden";
 		document.getElementById(element_name).checked = checked;
 		hideCol(i, checked);
 	}
-	var key = "com.pagetable.c64mem.column_decimal";
-	var element_name = "checkbox_decimal";
-	var visible = localStorage.getItem(key) == "visible";
-	document.getElementById(element_name).checked = visible;
-	toggleDecimal(visible);
+
+	if (config.has_decimal_column) {
+		var key = config.storage_prefix + "column_decimal";
+		var element_name = "checkbox_decimal";
+		var visible = localStorage.getItem(key) == "visible";
+		document.getElementById(element_name).checked = visible;
+		toggleDecimal(visible);
+	}
 }
 
 function toggleDecimal(visible) {
 	var tbl = document.getElementById("disassembly_table");
 	for (var i = 0; i < tbl.rows.length; i++) {
-		tbl.rows[i].cells[2].style.display = visible ? "" : "none";
+		tbl.rows[i].cells[config.decimal_column].style.display = visible ? "" : "none";
 	}
-	var key = "com.pagetable.c64mem.column_decimal";
+	var key = config.storage_prefix + "column_decimal";
 	var cnt = document.getElementById("disassembly_container");
 	if (visible) {
 		cnt.className = "disassembly_container_with_dec";
@@ -35,9 +60,9 @@ function toggleDecimal(visible) {
 function hideCol(col, checked) {
 	var tbl = document.getElementById("disassembly_table");
 	for (var i = 0; i < tbl.rows.length; i++) {
-		tbl.rows[i].cells[col+3].style.display = checked ? "" : "none";
+		tbl.rows[i].cells[col + config.number_of_header_columns].style.display = checked ? "" : "none";
 	}
-	var key = "com.pagetable.c64mem.column_" + col;
+	var key = config.storage_prefix + "column_" + col;
 	if (checked) {
 		localStorage.removeItem(key);
 	} else {
@@ -57,10 +82,10 @@ function openAll() {
 
 function closeAll() {
 	var elems = document.getElementsByTagName("details");
-	document.getElementById("toggle_details_button").setAttribute("onClick", "javascript: openAll();" );
+	document.getElementById("toggle_details_button").setAttribute("onClick", "javascript: openAll();");
 	document.getElementById("toggle_details_button").innerHTML = "Expand All Details";
 
 	for (let item of elems) {
-	   item.removeAttribute("open");
+		item.removeAttribute("open");
 	}
 }
